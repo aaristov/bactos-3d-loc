01cde0d89d0dcb4ee3ee24b878621300ca33e62e
Andrey Aristov <aaristov@pasteur.fr>
2025-01-27 15:01:32 +0100

    comply with the legacy axis order

--- src/bactos_3d_loc/crop.py
+++ src/bactos_3d_loc/crop.py
@@ -1,3 +1,4 @@
+import numpy as np
 import dask.array as da
 import pandas as pd
 import zarr
@@ -22,31 +23,32 @@ def extract_crops(
     zzz = zarr.open(
         output_zarr,
         mode='w', 
-        shape=(n_frames, n_droplets, n_chips, n_zplanes, crop_size, crop_size),
-        chunks=(1, 25, 1, 25, crop_size, crop_size),
+        shape=(n_chips, n_droplets, n_frames, n_zplanes, crop_size, crop_size),
+        chunks=(1, 50, 1, n_zplanes, crop_size, crop_size),
         dtype='u2'
     )
-    
+
     d = crop_size // 2
     assert len(dask_array) == n_frames
-    for f, frame in tqdm(enumerate(dask_array), desc="frame"):
+    for f, frame in enumerate(tqdm(dask_array, desc="frame")):
         zslices = []
         assert len(frame) == n_zplanes
 
-        for z in tqdm(frame, desc="z"):
+        for z, zplane in enumerate(tqdm(frame, desc="z")):
             crops = [], []
-            zslice = z.compute()
+            zslice = zplane.compute()
             assert len(droplets_df.values) == n_chips * n_droplets
-            assert len(droplets_df.values[0]) == 3 # chip, y ,x
+            assert len(droplets_df.values[0]) == 3  # chip, y ,x
             for droplet in droplets_df.values:
                 chip, y, x = map(int, droplet)
-                crops[chip].append(zslice[chip, y-d:y+d, x-d:x+d])
+                crop = zslice[chip, y-d:y+d, x-d:x+d]
+                crops[chip].append(crop)
             zslices.append(crops)
-
-        frame_data = da.array(zslices)  # (chip, z, droplet, y, x)
-        frame_data = frame_data.transpose(2, 1, 0, 3, 4)  # => (droplet, chip, z, y, x)
-        frame_data = frame_data.compute()
-        zzz[f] = frame_data
+        data = np.array(zslices).transpose(1, 2, 0, 3, 4)
+        if f == 0:
+            print(data.shape)
+        for c, crop in enumerate(data):
+            zzz[c, :, f] = crop
 
 
 def main():
